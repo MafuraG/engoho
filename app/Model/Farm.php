c6b00f4713df2489a2525e4dd347d08b82fca0cf
Gabriel Mafura <mafurag@genius-dynamics.com>
2018-05-13 10:47:59 +0300

    Debug seeding

--- app/Model/Farm.php
+++ app/Model/Farm.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Farm extends Model
 {
     //
+
+    public function lots(){
+        return $this->hasMany('App\Model\Lot');
+    }
 }
--- app/Model/Lot.php
+++ app/Model/Lot.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Lot extends Model
 {
     //
+    public function farm(){
+        return $this->belongsTo('App\Model\Lot');
+    }
 }
