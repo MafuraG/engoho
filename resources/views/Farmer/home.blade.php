7de253cb03e26166f7ccbe46bf812ab7255c1b9c
Gabriel Mafura <mafurag@genius-dynamics.com>
2018-05-17 16:08:57 +0300

    Farmers and Home page layout

--- resources/views/Farmer/home.blade.php
+++ resources/views/Farmer/home.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 <div class="container">
-    Farmer home        
+<div class="row justify-content-center">
+        <div class="card-deck">
+            <div class="card">
+                <h4 class="card-title text-center">Active Lots</h4>
+                <div class="card-body text-center d-flex flex-column">
+                <p class="card-text">This are a list of active lots of crops or animals in farm</p>
+                <a href="#" class="mt-auto btn btn-primary">Open</a>
+                </div>
+            </div>
+            <div class="card">
+                <h4 class="card-title text-center">Inventory</h4>
+                <div class="card-body text-center d-flex flex-column">
+                <p class="card-text">A simple inventory of farm inputs</p>
+                <a href="#" class="mt-auto btn btn-primary">Open</a>
+                </div>
+            </div>
+            <div class="card">
+                <h4 class="card-title text-center">Finance</h4>
+                <div class="card-body text-center d-flex flex-column">
+                <p class="card-text">Show how much has been spent and how much has been earned</p>
+                <a href="#" class="mt-auto btn btn-primary">Open</a>
+                </div>
+            </div>
+        </div>
+</div>    
 </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,40 +3,44 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="card-columns">
+        <div class="card-deck">
             <div class="card">
-              <h4 class="card-title text-center">Farmer</h4>
-              <div class="card-body text-center">
-                <p class="card-text">Some text inside the first card</p>
-                <a href="#" class="btn btn-primary">Open</a>
+              <div class="card-body text-center d-flex flex-column">
+                <h4 class="card-title text-center">Farmer</h4>
+                <p class="card-text">Some text inside the first card</p>   
+                <a  href="#" class="mt-auto btn btn-primary">Open</a>             
+              </div>   
+            </div>
+
+            <div class="card">
+              <div class="card-body text-center d-flex flex-column">
+                <h4 class="card-title text-center">Service providers</h4>
+                <p class="card-text">Some text inside the second card</p>   
+                <a  href="#" class="mt-auto btn btn-primary">Open</a>             
               </div>
             </div>
+
             <div class="card">
-              <h4 class="card-title text-center">Service providers</h4>
-              <div class="card-body text-center">
-                <p class="card-text">Some text inside the second card</p>
-                <a href="#" class="btn btn-primary">Open</a>
+              <div class="card-body text-center d-flex flex-column">
+                <h4 class="card-title text-center">Agro inputs supplier</h4>
+                <p class="card-text">Some text inside the third card</p>     
+                <a  href="#" class="mt-auto btn btn-primary">Open</a>           
               </div>
             </div>
+
             <div class="card">
-              <h4 class="card-title text-center">Agro inputs supplier</h4>
-              <div class="card-body text-center">
-                <p class="card-text">Some text inside the third card</p>
-                <a href="#" class="btn btn-primary">Open</a>
-              </div>
-            </div>
-            <div class="card">
-                <h4 class="card-title text-center">Agro Agents</h4>
-                <div class="card-body text-center">
-                    <p class="card-text">Some text inside the third card</p>
-                    <a href="#" class="btn btn-primary">Open</a>
+                <div class="card-body text-center d-flex flex-column">
+                    <h4 class="card-title text-center">Agro Agents</h4>
+                    <p class="card-text">Some text inside the third card</p>   
+                    <a  href="#" class="mt-auto btn btn-primary">Open</a>                 
                 </div>
             </div>  
+
             <div class="card">
-                <h4 class="card-title text-center">Consumers</h4>
-                <div class="card-body text-center">
-                    <p class="card-text">Some text inside the third card</p>
-                    <a href="#" class="btn btn-primary">Open</a>
+                <div class="card-body text-center d-flex flex-column">
+                    <h4 class="card-title text-center">Consumers</h4>
+                    <p class="card-text">Some text inside the third card</p>   
+                    <a href="#" class="mt-auto btn btn-primary ">Open</a>                 
                 </div>
             </div>
             
