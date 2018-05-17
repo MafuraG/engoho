3749699eb38e136448a98c72a3504d0dda08b159
Gabriel Mafura <mafurag@genius-dynamics.com>
2018-05-17 12:42:05 +0300

    Home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,44 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="card-columns">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+              <h4 class="card-title text-center">Farmer</h4>
+              <div class="card-body text-center">
+                <p class="card-text">Some text inside the first card</p>
+                <a href="#" class="btn btn-primary">Open</a>
+              </div>
+            </div>
+            <div class="card">
+              <h4 class="card-title text-center">Service providers</h4>
+              <div class="card-body text-center">
+                <p class="card-text">Some text inside the second card</p>
+                <a href="#" class="btn btn-primary">Open</a>
+              </div>
+            </div>
+            <div class="card">
+              <h4 class="card-title text-center">Agro inputs supplier</h4>
+              <div class="card-body text-center">
+                <p class="card-text">Some text inside the third card</p>
+                <a href="#" class="btn btn-primary">Open</a>
+              </div>
+            </div>
+            <div class="card">
+                <h4 class="card-title text-center">Agro Agents</h4>
+                <div class="card-body text-center">
+                    <p class="card-text">Some text inside the third card</p>
+                    <a href="#" class="btn btn-primary">Open</a>
+                </div>
+            </div>  
+            <div class="card">
+                <h4 class="card-title text-center">Consumers</h4>
+                <div class="card-body text-center">
+                    <p class="card-text">Some text inside the third card</p>
+                    <a href="#" class="btn btn-primary">Open</a>
                 </div>
             </div>
-        </div>
+            
+          </div>
     </div>
 </div>
 @endsection
